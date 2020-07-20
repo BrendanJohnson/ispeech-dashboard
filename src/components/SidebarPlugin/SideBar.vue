1ba4078bf027d33e808515ec763e844b304dba21
Brendan Johnson <brendan@neap.co>
2020-07-20 11:24:06 +0800

    Add ispeech logo and change info colour to ispeech theme

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -7,7 +7,7 @@
       <div class="logo">
         <a href="#" class="simple-text logo__container">
             <div class="logo-img">
-                <img src="img/vue-logo.png" alt="">
+                <img src="img/ispeech-logo-light-transparent.png" alt="">
             </div>
           {{title}}
         </a>
