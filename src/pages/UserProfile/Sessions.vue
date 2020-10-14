e4b7e77d18d9ba463ed38c9ddac73b2319d5129e
Brendan Johnson <brendan@neap.co>
2020-10-14 19:01:47 +0800

    Improved performance in Sessions screen by displaying Speech Sessions in collapsible accordion and tweaking options for Wavsurfer plugin

--- src/pages/UserProfile/Sessions.vue
+++ src/pages/UserProfile/Sessions.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div v-if="speechSessions.length">
     <!-- Delete modal -->
     <b-modal id="delete-modal" @ok="handleDeleteSession">
@@ -25,125 +24,126 @@
       :per-page="5"
       aria-controls="my-table"
     ></b-pagination>
-    <card v-for="(session, i) in speechSessions" :key="session.sessionId">
-      <h4 slot="header" class="card-title">
-        Session {{session.createdOn | formatDate}}
-            <b-dropdown class="float-right" id="view-data-dropdown" size="sm" text="Advanced">
-              <b-dropdown-item @click="downloadCsv(session.timelineUrl)">Download CSV</b-dropdown-item>
-              <b-dropdown-item @click="sessionToUpdate = session" v-b-modal="'upload-modal'">Upload CSV</b-dropdown-item>
-              <b-dropdown-item @click="showFileInNewWindow(session.manifestUrl)">View XML</b-dropdown-item>
-              <b-dropdown-item @click="showFileInNewWindow(session.timelineUrl)">View JSON</b-dropdown-item>
-              <b-dropdown-item v-b-modal="'delete-modal'" @click="sessionToDelete = session">Delete Session
-              </b-dropdown-item>
-            </b-dropdown>   
-      </h4>
-      <div :id="'waveform-session-' + session.sessionId" ref="waveform">
+    <div class="accordion"  role="tablist">
+    <b-card v-for="(session, i) in speechSessions" :key="session.sessionId">
+      <b-card-header header-tag="header" slot="header" role="tab" class="card-title">
+        {{session.audioFilename ? session.audioFilename.split('/')[1] : 'Session'}}  ({{session.createdOn | formatDate}})
+       <b-dropdown variant="link" id="view-data-dropdown" size="sm" text="Advanced">
+                <b-dropdown-item @click="downloadCsv(session.timelineUrl)">Download CSV</b-dropdown-item>
+                <b-dropdown-item @click="sessionToUpdate = session" v-b-modal="'upload-modal'">Upload CSV</b-dropdown-item>
+                <b-dropdown-item @click="showFileInNewWindow(session.manifestUrl)">View XML</b-dropdown-item>
+                <b-dropdown-item @click="showFileInNewWindow(session.timelineUrl)">View JSON</b-dropdown-item>
+                <b-dropdown-item v-b-modal="'delete-modal'" @click="sessionToDelete = session">Delete Session
+                </b-dropdown-item>
+        </b-dropdown> 
+        <a v-b-toggle class="float-right" :href="'#accordion-' + session.sessionId" @click.prevent><span class="when-open">Close</span><span class="when-closed">Open</span> Session</a>
+      </b-card-header>
+      <b-collapse accordion="my-accordion" :id="'accordion-' + session.sessionId" role="tabpanel">
+        <div :id="'waveform-session-' + session.sessionId" ref="waveform">
           <!-- Here be the waveform -->
         </div>
         <div id="wave-timeline" ref="wave-timeline">
         </div>
-
         <div>
-         <hr/>
-          <div class="row">
-            <div class="col-6">
-              <button class="btn btn-primary" @click="playMusic(session.sessionId)">
-                <i class="glyphicon glyphicon-play"></i>
-                Play
-                /
-                <i class="glyphicon glyphicon-pause"></i>
-                Pause
-              </button>
+          <hr/>
+            <div class="row">
+              <div class="col-6">
+                <button class="btn btn-primary" @click="playMusic(session.sessionId)">
+                  <i class="glyphicon glyphicon-play"></i>
+                  Play
+                  /
+                  <i class="glyphicon glyphicon-pause"></i>
+                  Pause
+                </button>
+              </div>  
+              <div class="col-6">
+                <b-form-input v-model="annotationsFilter"  placeholder="Type to Search"></b-form-input>
+              </div>
             </div>
-            <div class="col-6">
-              <b-form-input v-model="annotationsFilter"  placeholder="Type to Search"></b-form-input>
-            </div>
-          </div>
-      </div>
-    <div class="row">
-      <div class="col-12">
-
+        </div>
+        <div class="row">
+          <div class="col-12">
             <a v-b-toggle class="float-right" href="#example-collapse" @click.prevent>Session Statistics</a>
-       
           </div>
-      </div>
-       <b-collapse id="example-collapse">
-      <b-card title="Word types">
-      <div>
-        <p v-for="(count, tag) in session.tagTotals">{{tag}}: {{count}}</p>
         </div>
-      </b-card>
-      </b-collapse>
-    <div id="annotations-table-container">
-        <div class="justify-content-center row">
-
-        </div>
-        <b-table  v-if="session.loaded" :filter="annotationsFilter" :id="'annotations-session-' + session.sessionId" ref="annotationsTable" primary-key="key" sticky-header striped hover :items="items[session.sessionId]" :fields="fields" @row-clicked="row=>clickRow(row, session.sessionId)">
+        <b-collapse id="example-collapse">
+          <b-card title="Word types">
+            <div>
+              <p v-for="(count, tag) in session.tagTotals">{{tag}}: {{count}}</p>
+            </div>
+          </b-card>
+        </b-collapse>
+        <div id="annotations-table-container">
+          <div class="justify-content-center row">
+          </div>
+          <b-table v-if="session.loaded" :filter="annotationsFilter" :id="'annotations-session-' + session.sessionId" ref="annotationsTable" primary-key="key" sticky-header striped hover :items="items[session.sessionId]" :fields="fields" @row-clicked="row=>clickRow(row, session.sessionId)">
             <template v-slot:cell(transcript)="row">
-            		<p v-if="row.item.alignable_id == editingRow.alignable_id"><input v-model="row.item.transcript"  @blur="saveTranscriptRow(row, session.sessionId)"></input></p>
-            		<p v-else>{{row.item.transcript}}</p>
-                <div v-if="row.item.syntaxValues">
-                  <syntax-canvas style="width: 100%; height: 35px;">
-                    <syntax-arrow
-                    v-for="obj, index of row.item.syntaxValues"
-                    :x1="((index / row.item.syntaxValues.length) * 100)"
-                    :x2="((index / row.item.syntaxValues.length) * 100) + (100 / row.item.syntaxValues.length)"
-                    :y1="100"
-                    :y2="100"
-                    :skip="obj.skip"
-                    :edgeLabel="obj.edgeLabel"
-                    >
-                    </syntax-arrow>
-                  </syntax-canvas>
-                  <syntax-canvas style="width: 100%; height: 50px;">
-                    <syntax-box
-                      v-for="obj, index of row.item.syntaxValues"
-                      :x1="((index / row.item.syntaxValues.length) * 100)"
-                      :x2="((index / row.item.syntaxValues.length) * 100) + (100 / row.item.syntaxValues.length)"
-                      :y1="50"
-                      :y2="40"
-                      :color="obj.color"
-                      :label="obj.label"
-                      :tagLabel="obj.tag"
-                      :detailLabel="obj.person"
-                    >
-                    </syntax-box>
-                  </syntax-canvas>  
-                </div>
-                <b-button-toolbar class="float-right float-top">
-                  <b-button-group class="btn-group-sm">
-                    <b-button variant="outline-primary">
-                      <b-icon-question-circle size="sm" scale="1" class="float-right" @click="analyzeText(row, session.sessionId)" >
-                    </b-icon-question-circle>
-                  </b-button>
-                    <b-button variant="outline-primary">
-                      <b-icon-file-plus size="sm" scale="1" class="float-right" @click="toggleEdit($event, row)">
-                    </b-icon-file-plus>
-                  </b-button>
-                  <b-button variant="outline-primary">
-                   
-                <b-icon-mic size="sm" scale="1" class="float-right" @click="toggleEdit($event, row, true)">
-                </b-icon-mic>
-                  </b-button>
-                </b-button-group>
-                </b-button-toolbar>
-        
-            </template>
-            <template v-slot:cell(sentiment)="row">
-                <b-icon :icon="'emoji-' + row.item.sentiment" scale="1.4" variant="primary"></b-icon>
-            </template>
-            <template v-slot:cell(star)="row">
-                <div @click="toggleStar($event, row, session.sessionId)" class="mb-0">
-                  <b-icon-star-fill v-if="row.item.starred" scale="1.4" variant="primary"></b-icon-star-fill>
-                  <b-icon-star v-else scale="1.4" variant="primary"></b-icon-star>
-                </div>
-            </template>
-        </b-table>
-      </div>
-    <div id="annotations" class="table-responsive" style="display:none;">
-      <!-- Hidden as we want to use our own table -->
+                  		<p v-if="row.item.alignable_id == editingRow.alignable_id"><input v-model="row.item.transcript"  @blur="saveTranscriptRow(row, session.sessionId)"></input></p>
+                  		<p v-else>{{row.item.transcript}}</p>
+                      <div v-if="row.item.syntaxValues">
+                        <syntax-canvas style="width: 100%; height: 35px;">
+                          <syntax-arrow
+                          v-for="obj, index of row.item.syntaxValues"
+                          :x1="((index / row.item.syntaxValues.length) * 100)"
+                          :x2="((index / row.item.syntaxValues.length) * 100) + (100 / row.item.syntaxValues.length)"
+                          :y1="100"
+                          :y2="100"
+                          :skip="obj.skip"
+                          :edgeLabel="obj.edgeLabel"
+                          >
+                          </syntax-arrow>
+                        </syntax-canvas>
+                        <syntax-canvas style="width: 100%; height: 50px;">
+                          <syntax-box
+                            v-for="obj, index of row.item.syntaxValues"
+                            :x1="((index / row.item.syntaxValues.length) * 100)"
+                            :x2="((index / row.item.syntaxValues.length) * 100) + (100 / row.item.syntaxValues.length)"
+                            :y1="50"
+                            :y2="40"
+                            :color="obj.color"
+                            :label="obj.label"
+                            :tagLabel="obj.tag"
+                            :detailLabel="obj.person"
+                          >
+                          </syntax-box>
+                        </syntax-canvas>  
+                      </div>
+                      <b-button-toolbar class="float-right float-top">
+                        <b-button-group class="btn-group-sm">
+                          <b-button variant="outline-primary">
+                            <b-icon-question-circle size="sm" scale="1" class="float-right" @click="analyzeText(row, session.sessionId)" >
+                          </b-icon-question-circle>
+                        </b-button>
+                          <b-button variant="outline-primary">
+                            <b-icon-file-plus size="sm" scale="1" class="float-right" @click="toggleEdit($event, row)">
+                          </b-icon-file-plus>
+                        </b-button>
+                        <b-button variant="outline-primary">
+                         
+                      <b-icon-mic size="sm" scale="1" class="float-right" @click="toggleEdit($event, row, true)">
+                      </b-icon-mic>
+                        </b-button>
+                      </b-button-group>
+                      </b-button-toolbar>
+              
+                  </template>
+                  <template v-slot:cell(sentiment)="row">
+                      <b-icon :icon="'emoji-' + row.item.sentiment" scale="1.4" variant="primary"></b-icon>
+                  </template>
+                  <template v-slot:cell(star)="row">
+                      <div @click="toggleStar($event, row, session.sessionId)" class="mb-0">
+                        <b-icon-star-fill v-if="row.item.starred" scale="1.4" variant="primary"></b-icon-star-fill>
+                        <b-icon-star v-else scale="1.4" variant="primary"></b-icon-star>
+                      </div>
+                  </template>
+          </b-table>
+        </div>
+        <div id="annotations" class="table-responsive" style="display:none;">
+          <!-- Hidden as we want to use our own table -->
+        </div>
+      </b-collapse>
+    </b-card>
     </div>
-    </card>
   </div>
   <div v-else>
     <p class="no-results">No matching sessions were found</p>
@@ -153,7 +153,7 @@
   </div>
 </template>
 <script>
-  import { BIcon, BIconEmojiSmile, BIconEmojiNeutral, BIconEmojiFrown, BIconEmojiDizzy, BIconEmojiLaughing, BIconQuestionCircle, BButtonToolbar, BIconFilePlus, BIconMic, BIconStar, BIconStarFill } from 'bootstrap-vue'
+  import { BCard, BCollapse, BIcon, BIconEmojiSmile, BIconEmojiNeutral, BIconEmojiFrown, BIconEmojiDizzy, BIconEmojiLaughing, BIconQuestionCircle, BButtonToolbar, BIconFilePlus, BIconMic, BIconStar, BIconStarFill } from 'bootstrap-vue'
   import Card from 'src/components/Cards/Card.vue'
   import SyntaxArrow from 'src/components/Syntax/SyntaxArrow.vue'
   import SyntaxBox from 'src/components/Syntax/SyntaxBox.vue'
@@ -324,6 +324,20 @@
       let speechRecognitionBlock = this.speechRecognitionBlock;
       let speechRecognitionText = this.speechRecognitionText;
      
+      this.$root.$on('bv::collapse::state', (collapseId, isJustShown) => {
+        let sessionId = collapseId.split("accordion-")[1];
+        // Find the corresponding speech session
+        let session = this.speechSessions.filter((session) => {
+          return session.sessionId == sessionId;
+        })[0];
+        if (isJustShown) {
+          this.renderWavesurfer(session)
+        }
+        else {
+          this.wavesurfers[session.sessionId].destroy();
+        }
+      })
+
       this.socket.on('nextBlock', data => {
         speechRecognitionBlock++
       })
@@ -348,9 +362,6 @@
           };
       })
 
-    	if (this.speechSessions.length) {
-      	this.speechSessions.forEach(session => this.renderWavesurfer(session))
-    	}
     },
     computed: {
       ...mapGetters({ speechSessions: 'getSpeechSessionsPaginated' }),
@@ -365,11 +376,7 @@
       speechSessions(sessions) {
         if (sessions.length) {
           sessions.forEach(session => {
-            if(!this.wavesurfers[session.sessionId]) {
-              this.renderWavesurfer(session)
-            }
-            else {            
-
+            if (this.wavesurfers[session.sessionId]) {            
               this.items[session.sessionId] = this.items[session.sessionId].map(item => {
                 return itemMapper(session, session.annotations[item.alignable_id], item.alignable_id);
               })
@@ -417,7 +424,6 @@
           var container = this.$refs.waveform.filter(item => {
             return item.id == 'waveform-session-' + session.sessionId 
           })[0];
-          console.log('CREATING WAVESURFER FOR: ' + session.sessionId);
           // Create empty session
           if (!this.items[session.sessionId]) {
             this.items[session.sessionId] = [];
@@ -425,11 +431,14 @@
 
           this.wavesurfers[session.sessionId] = WaveSurfer.create({
             container: container,
-             barWidth: 2,
+            barWidth: 2,
             barHeight: 1, // the height of the wave
+            closeAudioContext: true,
             waveColor: '#409EFF',
             progressColor: 'blue',
             minPxPerSec: 10, // for scrolling
+            partialRender: true,
+            pixelRatio: 1,
             scrollParent: true,  // for scrolling
             backend: 'MediaElement',
             mediaControls: false,
